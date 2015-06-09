9559cd39f629f6ba1c2d3b163766ca071095065f
Joweria Nassanga <jowerialawi@gmail.com>
2015-06-09 12:40:16 +0300

    tracking the car variables

---
+++ car/src/main/java/com/terasoft/rnd/gitprac/car/Car.java
@@ -0,0 +1,73 @@
+package com.terasoft.rnd.gitprac.car;
+
+public class Car {
+
+	private String engine_sys, power_train, braking_sys, steering, suspension, tyres, electircal_sys, exhaust_sys;
+
+	public String getEngine_sys() {
+		return engine_sys;
+	}
+
+	public void setEngine_sys(String engine_sys) {
+		this.engine_sys = engine_sys;
+	}
+
+	public String getPower_train() {
+		return power_train;
+	}
+
+	public void setPower_train(String power_train) {
+		this.power_train = power_train;
+	}
+
+	public String getBraking_sys() {
+		return braking_sys;
+	}
+
+	public void setBraking_sys(String braking_sys) {
+		this.braking_sys = braking_sys;
+	}
+
+	public String getSteering() {
+		return steering;
+	}
+
+	public void setSteering(String steering) {
+		this.steering = steering;
+	}
+
+	public String getSuspension() {
+		return suspension;
+	}
+
+	public void setSuspension(String suspension) {
+		this.suspension = suspension;
+	}
+
+	public String getTyres() {
+		return tyres;
+	}
+
+	public void setTyres(String tyres) {
+		this.tyres = tyres;
+	}
+
+	public String getElectircal_sys() {
+		return electircal_sys;
+	}
+
+	public void setElectircal_sys(String electircal_sys) {
+		this.electircal_sys = electircal_sys;
+	}
+
+	public String getExhaust_sys() {
+		return exhaust_sys;
+	}
+
+	public void setExhaust_sys(String exhaust_sys) {
+		this.exhaust_sys = exhaust_sys;
+	}
+	
+	
+	
+}

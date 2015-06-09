e48f7dbcd4dc75a5b453b0176bdb1ca86b26079a
Joweria Nassanga <jowerialawi@gmail.com>
2015-06-09 12:58:30 +0300

    added engine interface v1

---
+++ car/src/main/java/com/terasoft/rnd/gitprac/car/engine/IEngine.java
@@ -0,0 +1,23 @@
+package com.terasoft.rnd.gitprac.car.engine;
+
+public interface IEngine {
+
+	public String Camshaft = "",
+					valve_cover= "",
+					intake_valve= "",
+					head= "",
+					intake_port= "",
+					coolant= "",
+					engine_block= "",
+					oil_pan= "",
+					oil_sump= "",
+					spark_plug= "",
+					exhaust_valve= "",
+					exhaust_port= "",
+					piston= "",
+					connecting_rod= "",
+					rod_bearing= "",
+					crankshaft = "";
+					
+	
+}

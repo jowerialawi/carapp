c6d5f3d80c03b4d0f9c900a82fec1e343eaf560b
Joweria Nassanga <jowerialawi@gmail.com>
2015-06-09 12:27:16 +0300

    Initial commit of carapp

---
+++ car/src/main/java/com/terasoft/rnd/gitprac/car/App.java
@@ -0,0 +1,13 @@
+package com.terasoft.rnd.gitprac.car;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
